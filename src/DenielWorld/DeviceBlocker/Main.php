87e0de735629e453ad1b7642a0de0f64dcbf0e96
NovaStark1245 <95739044+NovaStark1234@users.noreply.github.com>
2022-03-24 16:07:36 +0700

    Update Main.php

--- src/DenielWorld/DeviceBlocker/Main.php
+++ src/DenielWorld/DeviceBlocker/Main.php
@@ -9,10 +9,13 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 use pocketmine\network\mcpe\JwtUtils;
+use pocketmine\event\player\PlayerLoginEvent;
 
 class Main extends PluginBase implements Listener{
 
-    public function onEnable()
+    public $deviceOS = [];
+
+    protected function onEnable() :void
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder());
@@ -27,28 +30,39 @@ class Main extends PluginBase implements Listener{
     }
 
     public function onPacketReceive(DataPacketReceiveEvent $event) : void {
+        //$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        if($event->getPacket() instanceof LoginPacket) {
+            //var_dump(JwtUtils::parse($event->getPacket()->clientDataJwt)[1]);
+            $this->deviceOS[JwtUtils::parse($event->getPacket()->clientDataJwt)[1]["ThirdPartyName"]] = JwtUtils::parse($event->getPacket()->clientDataJwt)[1]["DeviceOS"];
+        }
+    }
+
+    public function onLogin(PlayerLoginEvent $event) {
         $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        if($event->getPacket() instanceof LoginPacket){
-            //1 = Android, 2 = IOS, 7 = W10
-            /*if($event->getPacket()->clientData["DeviceOS"] == 7){
-                if($cfg->get("w10") == true){
-                    $this->getServer()->broadcastMessage(self::replaceText(TF::colorize($cfg->get("w10-quit-message")), $event->getPlayer()->getName()));
-                    $event->getPlayer()->kick(TF::colorize($cfg->get("w10-kick-reason")), false);
-                }
+        if(!$event->getPlayer()->isOnline()) {
+            unset($this->deviceOS[$event->getPlayer()->getName()]);
+            return;
+        }
+        if($this->deviceOS[$event->getPlayer()->getName()] == 7){
+            if($cfg->get("w10") == true){
+                $this->getServer()->broadcastMessage(self::replaceText(TF::colorize($cfg->get("w10-quit-message")), $event->getPlayer()->getName()));
+                $event->getPlayer()->kick(TF::colorize($cfg->get("w10-kick-reason")), false);
+                unset($this->deviceOS[$event->getPlayer()->getName()]);
             }
-            elseif($event->getPacket()->clientData["DeviceOS"] == 1){
-                if($cfg->get("android") == true){
-                    $this->getServer()->broadcastMessage(self::replaceText(TF::colorize($cfg->get("android-quit-message")), $event->getPlayer()->getName()));
-                    $event->getPlayer()->kick(TF::colorize($cfg->get("android-kick-reason")), false);
-                }
+        }
+        elseif($this->deviceOS[$event->getPlayer()->getName()] == 1){
+            if($cfg->get("android") == true){
+                $this->getServer()->broadcastMessage(self::replaceText(TF::colorize($cfg->get("android-quit-message")), $event->getPlayer()->getName()));
+                $event->getPlayer()->kick(TF::colorize($cfg->get("android-kick-reason")), false);
+                unset($this->deviceOS[$event->getPlayer()->getName()]);
             }
-            elseif($event->getPacket()->clientData["DeviceOS"] == 2){
-                if($cfg->get("ios") == true){
-                    $this->getServer()->broadcastMessage(self::replaceText(TF::colorize($cfg->get("ios-quit-message")), $event->getPlayer()->getName()));
-                    $event->getPlayer()->kick(TF::colorize($cfg->get("ios-kick-reason")), false);
-                }
-            }*/
-            var_dump(JwtUtils::parse($packet->clientDataJwt));
+        }
+        elseif($this->deviceOS[$event->getPlayer()->getName()] == 2){
+            if($cfg->get("ios") == true){
+                $this->getServer()->broadcastMessage(self::replaceText(TF::colorize($cfg->get("ios-quit-message")), $event->getPlayer()->getName()));
+                $event->getPlayer()->kick(TF::colorize($cfg->get("ios-kick-reason")), false);
+                unset($this->deviceOS[$event->getPlayer()->getName()]);
+            }
         }
     }
 }
